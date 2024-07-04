a95d0283ddf335b163666ce54b3d19b7ccf010ce
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-07-04 14:47:13 +0400

    Initial Commit

---
+++ E-swavle/E_swavleApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_swavleApp.swift
+//  E-swavle
+//
+//  Created by Ana on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct E_swavleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
